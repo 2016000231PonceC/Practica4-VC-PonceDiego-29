200c65fa89666cb6f47c14d0b4aaf4eee4816431
LABORATORIO <LABORATORIO@LAB-A-PC28>
2025-02-18 11:57:59 -0600

    Agregar archivos de proyecto.

---
+++ Practica4-VC-PonceDiego-29/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Practica4_VC_PonceDiego_29
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.Matriz1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 42);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 95);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(182, 42);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 95);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(12, 192);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 95);
+            this.listBox3.TabIndex = 2;
+            // 
+            // Matriz1
+            // 
+            this.Matriz1.Location = new System.Drawing.Point(12, 12);
+            this.Matriz1.Name = "Matriz1";
+            this.Matriz1.Size = new System.Drawing.Size(75, 23);
+            this.Matriz1.TabIndex = 3;
+            this.Matriz1.Text = "Matriz1";
+            this.Matriz1.UseVisualStyleBackColor = true;
+            this.Matriz1.Click += new System.EventHandler(this.Matriz1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(182, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Matriz2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 163);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Suma";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(68, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Resta";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(124, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Multiplicacion";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(182, 163);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(50, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Division";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(238, 163);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(50, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Cuadrado";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(294, 163);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(50, 23);
+            this.button7.TabIndex = 10;
+            this.button7.Text = "Cubo";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(350, 163);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(50, 23);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "Raiz";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(422, 409);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Matriz1);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+        private System.Windows.Forms.Button Matriz1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+    }
+}
+
---
+++ Practica4-VC-PonceDiego-29/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+
+namespace Practica4_VC_PonceDiego_29
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int[,] m1;
+        private int[,] m2;
+        private int[,] suma;
+        private int[,] resta;
+        private int[,] multiplicacion;
+        private int[,] division;
+      
+   
+        private void Matriz1_Click(object sender, EventArgs e)
+        {
+            m1 = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++) 
+                {
+                    m1[i, c]= Convert.ToInt32(Interaction.InputBox("Ingrese un Numero"));
+                    listBox1.Items.Add(m1[i, c]);
+                
+                }
+            
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            m2 = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    m2[i, c] = Convert.ToInt32(Interaction.InputBox("Ingrese un Numero"));
+                    listBox2.Items.Add(m2[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            suma = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    suma[i, c] = m1[i, c]+ m2[i, c];
+                    listBox3.Items.Add(suma[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resta = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    resta[i, c] = m1[i, c] - m2[i, c];
+                    listBox3.Items.Add(resta[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            multiplicacion = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    multiplicacion[i, c] = m1[i, c] * m2[i, c];
+                    listBox3.Items.Add(multiplicacion[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            division = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    division[i, c] = m1[i, c] / m2[i, c];
+                    listBox3.Items.Add(division[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            cuadrado = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    cuadrado[i, c] = m1[i, c] ^ m2[i, c];
+                    listBox3.Items.Add(cuadrado[i, c]);
+
+                }
+
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            cubo = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    cubo[i, c] = m1[i, c] + m2[i, c];
+                    listBox3.Items.Add(cubo[i, c]);
+
+                }
+
+            }
+        }
+        private double[,] raiz1;
+        private void button8_Click(object sender, EventArgs e)
+        {
+            raiz1 = new double[3, 4];
+            m1 = new int[3, 4];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int c = 0; c < 4; c++)
+                {
+                    raiz1[i, c] = Math.Sqrt(m1[3, 4]);
+                    listBox3.Items.Add(raiz[i, c]);
+
+                }
+
+            }
+        }
+    }
+}
